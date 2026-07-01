df4eefe379bf49c1db532079eaa915c03834391b
Isabel Araujo <166257491+isabel1993araujo@users.noreply.github.com>
2026-07-01 18:51:40 -0300

    feat: implementa selection sort

--- src/basicos/selection.c
+++ src/basicos/selection.c
@@ -1,3 +1,17 @@
 void selectionSort(int v[], int n) {
+    int i, j, min_idx, temp;
 
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+
+        for (j = i + 1; j < n; j++) {
+            if (v[j] < v[min_idx]) {
+                min_idx = j;
+            }
+        }
+
+        temp = v[min_idx];
+        v[min_idx] = v[i];
+        v[i] = temp;
+    }
 }
