594b388248dc6601d1c6e2aa2c778d9b32bfbd9c
Isabel Araujo <166257491+isabel1993araujo@users.noreply.github.com>
2026-07-01 18:51:13 -0300

    feat: implementa bubble sort

--- src/basicos/bubble.c
+++ src/basicos/bubble.c
@@ -1,3 +1,13 @@
 void bubbleSort(int v[], int n) {
+    int i, j, temp;
 
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (v[j] > v[j + 1]) {
+                temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
+    }
 }
