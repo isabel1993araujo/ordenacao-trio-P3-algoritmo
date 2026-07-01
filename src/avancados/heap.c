b2c1fc3b8eb458e9e69a7a7c1fdc71a5a33b8699
Isabel Araujo <166257491+isabel1993araujo@users.noreply.github.com>
2026-07-01 18:54:38 -0300

    feat: implementa heap sort

--- src/avancados/heap.c
+++ src/avancados/heap.c
@@ -1,3 +1,38 @@
-void heapSort(int v[], int n) {
+static void heapify(int v[], int n, int i) {
+    int maior = i;
+    int esquerda = 2 * i + 1;
+    int direita = 2 * i + 2;
+    int temp;
 
+    if (esquerda < n && v[esquerda] > v[maior]) {
+        maior = esquerda;
+    }
+
+    if (direita < n && v[direita] > v[maior]) {
+        maior = direita;
+    }
+
+    if (maior != i) {
+        temp = v[i];
+        v[i] = v[maior];
+        v[maior] = temp;
+
+        heapify(v, n, maior);
+    }
+}
+
+void heapSort(int v[], int n) {
+    int i, temp;
+
+    for (i = n / 2 - 1; i >= 0; i--) {
+        heapify(v, n, i);
+    }
+
+    for (i = n - 1; i > 0; i--) {
+        temp = v[0];
+        v[0] = v[i];
+        v[i] = temp;
+
+        heapify(v, i, 0);
+    }
 }
