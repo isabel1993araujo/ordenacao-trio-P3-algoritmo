b8d2c336b65e93a361a8c204b20f05210eecd4b3
Isabel Araujo <166257491+isabel1993araujo@users.noreply.github.com>
2026-07-01 18:53:56 -0300

    feat: implementa quick sort

--- src/avancados/quick.c
+++ src/avancados/quick.c
@@ -1,3 +1,34 @@
-void quickSort(int v[], int n) {
+int partition(int v[], int low, int high) {
+    int pivot = v[high];
+    int i = low - 1;
+    int j, temp;
 
+    for (j = low; j < high; j++) {
+        if (v[j] < pivot) {
+            i++;
+
+            temp = v[i];
+            v[i] = v[j];
+            v[j] = temp;
+        }
+    }
+
+    temp = v[i + 1];
+    v[i + 1] = v[high];
+    v[high] = temp;
+
+    return i + 1;
+}
+
+void quickSortRec(int v[], int low, int high) {
+    if (low < high) {
+        int pi = partition(v, low, high);
+
+        quickSortRec(v, low, pi - 1);
+        quickSortRec(v, pi + 1, high);
+    }
+}
+
+void quickSort(int v[], int n) {
+    quickSortRec(v, 0, n - 1);
 }
